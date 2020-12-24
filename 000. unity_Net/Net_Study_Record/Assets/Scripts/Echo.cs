80f0d448f5b7321ae4fd0f71ed90f916768e3a87
getailin <getl@daoyuntech.com>
2020-12-24 18:02:17 +0800

    1 2020年12月24日18:02:09

---
+++ 000. unity_Net/Net_Study_Record/Assets/Scripts/Echo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Echo : MonoBehaviour
+{
+    /// <summary>
+    /// 套接字
+    /// </summary>
+    private Socket socket;
+
+
+    public Text text;
+
+    public InputField inputFeld;
+
+    /// <summary>
+    /// 连接按钮触发事件
+    /// </summary>
+    public void Connection()
+    {
+        //Socket
+        socket = new Socket(
+            AddressFamily.InterNetwork
+            , SocketType.Stream
+            , ProtocolType.Tcp);
+        
+        
+        //Connect
+        socket.Connect("127.0.0.1",8888);
+    }
+
+    /// <summary>
+    /// 点击发送按钮
+    /// </summary>
+    public void Send()
+    {
+        //Send
+        string sendStr = inputFeld.text;
+
+        byte[] bytes = System.Text.Encoding.Default.GetBytes(sendStr);
+
+        socket.Send(bytes);
+        
+        //Recv
+        byte[] readBuff = new byte[1024];
+
+        int count = socket.Receive(readBuff);
+        
+        string s = System.Text.Encoding.Default.GetString(readBuff,0,count);
+
+        text.text = s;
+        
+        //Close
+        socket.Close();
+    }
+    
+}
